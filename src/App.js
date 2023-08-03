b51ecb624701463e9b427c7b65b331e186cccc89
ayvazzenes <karaosman14@hotmail.com>
2023-08-03 13:47:40 +0300

    React-case-study

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import dataList from "./data.json";
+import Grid from "./components/Grid";
+import { useEffect,useState } from "react";
+
+function control(today, limit, dataList) {
+  let count = 0;
+
+  dataList.forEach((item) => {
+    const mailReceivedDate = new Date(item.mailReceivedDate);
+    const solutionSentDate = item.solutionSentDate ? new Date(item.solutionSentDate) : today;
+    const differenceInDays = Math.ceil((solutionSentDate.getTime() - mailReceivedDate.getTime()) / (1000 * 3600 * 24));
+    if (differenceInDays <= limit && item.isBackgroundColorRed) {
+      count++;
+    }
+  });
+
+  return count;
+}
+
+
 
 function App() {
+  let sourceProp = dataList;
+  const today = new Date('2021-10-06');
+  const limit = 5;
+  const count= control(today,limit,sourceProp);
+  const [dataTableDom, setDataTableDom] = useState([]);
+  useEffect(() => {
+    
+    const table = document.getElementById("personsTable");
+    const tableRows = Array.from(table.getElementsByTagName("tr"));
+
+    
+    const data = tableRows.map((row) => ({
+      name: row.cells[0].textContent,
+      mailReceivedDate: row.cells[1].textContent,
+      solutionSentDate: row.cells[2].textContent,
+      isBackgroundColorRed: false,
+    }));
+
+
+    setDataTableDom(data);
+  }, []);
+  console.log(dataTableDom);
+  
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Dgpays Case Study </h1>
+      <p>Hatalı boyanmış satır sayısı:{count}</p>
+      <Grid source={sourceProp} />
     </div>
   );
 }
---
+++ src/components/Grid.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Grid = ({ source }) => {
+  return (
+    <table id="personsTable">
+      <tbody>
+        {source.map((item, index) => (
+          <tr key={index} style={{ backgroundColor: item.isBackgroundColorRed ? "red" : "transparent" }}>
+            <td>{item.name}</td>
+            <td>{item.mailReceivedDate}</td>
+            <td>{item.solutionSentDate}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default Grid;
